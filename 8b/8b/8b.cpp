60586c1453d0f3dbbcb83ccf23dabd7134bc399c
dmusial98 <32642504+dmusial98@users.noreply.github.com>
2019-03-06 21:25:53 +0100

    8b f.v.

--- 8b/8b/8b.cpp
+++ 8b/8b/8b.cpp
@@ -3,8 +3,6 @@
 #include <fstream>
 #include <vector>
 
-int summary = 0;
-
 class Tree {
 public:
 	int num_of_nodes = 0;
@@ -12,6 +10,7 @@ public:
 	std::vector<Tree*> nodes;
 	std::vector<int> values;
 
+	friend int count(Tree *tree);
 	friend Tree* add_node(Tree *tr, std::fstream &file);
 };
 
@@ -21,7 +20,7 @@ Tree* add_node(Tree *tr, std::fstream &file)
 	file >> tr->num_of_nodes;
 	file >> tr->num_of_values;
 
-	Tree *new_tr = new Tree[tr->num_of_nodes];
+	Tree *new_tr = new Tree [tr->num_of_nodes];
 
 	for (int i = 0; i < tr->num_of_nodes; i++) {
 		tr->nodes.push_back(add_node(&new_tr[i], file));
@@ -32,10 +31,36 @@ Tree* add_node(Tree *tr, std::fstream &file)
 	for (int i = 0; i < tr->num_of_values; i++) {
 		file >> buf;
 		tr->values.push_back(buf);
-		summary += buf;
 	}
 
 	return tr;
+}
+
+int count(Tree *tree) {
+
+	int number = 0;
+
+	if (tree->num_of_nodes == 0) 
+	{
+		for (int i = 0; i < tree->num_of_values; i++) 
+		{
+			number += tree->values[i];
+		}
+	}
+	else
+	{
+		for (int i = 0; i < tree->num_of_values; i++) 
+		{
+			if (tree->values[i] <= tree->num_of_nodes) //zliczanie tego co siê da
+			{
+				number += count(tree->nodes[tree->values[i] - 1]);
+			}
+			
+		}
+	}
+
+	return number;
+
 }
 
 int main() {
@@ -45,9 +70,11 @@ int main() {
 
 	Tree* tr = new Tree;
 	add_node(tr, file);
-	
+
 	file.close();
-	std::cout << summary;
+	std::cout << count(tr);
 }
+
+
 
 
